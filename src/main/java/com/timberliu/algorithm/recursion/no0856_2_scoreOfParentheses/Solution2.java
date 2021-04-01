27bc7bcd2fa0f8666727b9d5faa8e03137fba242
liujie0813 <18220747087@163.com>
2021-04-01 19:02:17 +0800

    recursion - 394、726、856 -- 3 - 97

---
+++ src/main/java/com/timberliu/algorithm/recursion/no0856_2_scoreOfParentheses/Solution2.java
@@ -0,0 +1,42 @@
+package com.timberliu.algorithm.recursion.no0856_2_scoreOfParentheses;
+
+/**
+ * 856. 括号的分数
+ *
+ * Created by liujie on 2021/4/1
+ */
+
+public class Solution2 {
+
+    public static int scoreOfParentheses(String str) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                bal++;
+            } else {
+                bal--;
+                if (str.charAt(i - 1) == '(') {
+                    // 1 << x  ====  2^x
+                    ans += 1 << bal;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("----- https://leetcode-cn.com/problems/score-of-parentheses/ -----");
+        System.out.println("----- 1 -----");
+        System.out.println(scoreOfParentheses("()"));
+
+        System.out.println("----- 2 -----");
+        System.out.println(scoreOfParentheses("(())"));
+
+        System.out.println("----- 3 -----");
+        System.out.println(scoreOfParentheses("()()"));
+
+        System.out.println("----- 4 -----");
+        System.out.println(scoreOfParentheses("(()(()))"));
+
+    }
+}
