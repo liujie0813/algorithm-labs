449ce3d3d6b0969bb9a060f5b2cee4533989ff6e
liujie0813 <18220747087@163.com>
2021-04-28 15:10:06 +0800

    hashtable - 128、217、219、220、387、594 -- 6 - 107

---
+++ src/main/java/com/timberliu/algorithm/hashtable/no0128_3_longestConsecutiveSequence/Solution2.java
@@ -0,0 +1,52 @@
+package com.timberliu.algorithm.hashtable.no0128_3_longestConsecutiveSequence;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 128. 最长连续序列
+ *
+ *   DP
+ *
+ *     单次遍历
+ *     1.
+ *
+ * Created by liujie on 2021/4/28
+ */
+
+public class Solution2 {
+
+    public static int longestConsecutive(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int longest = 0;
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                int left = map.getOrDefault(num - 1, 0);
+                int right = map.getOrDefault(num + 1, 0);
+                int curLength = 1 + left + right;
+                if (curLength > longest) {
+                    longest = curLength;
+                }
+                map.put(num, curLength);
+                // num -> 该元素所在序列的长度
+                // 更新当前序列两端的值即可
+                map.put(num - left, curLength);
+                map.put(num + right, curLength);
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("----- https://leetcode-cn.com/problems/longest-consecutive-sequence/ -----");
+        System.out.println("----- 1 -----");
+        System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2}));
+
+        System.out.println("----- 2 -----");
+        System.out.println(longestConsecutive(new int[]{0,3,7,2,5,8,4,6,0,1}));
+
+    }
+
+}
---
+++ src/main/java/com/timberliu/algorithm/hashtable/no0217_1_containsDuplicate/related/no0220_2_containsDuplicate3/Solution2.java
@@ -0,0 +1,74 @@
+package com.timberliu.algorithm.hashtable.no0217_1_containsDuplicate.related.no0220_2_containsDuplicate3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ * 220. 存在重复元素 III
+ *
+ *  时间复杂度：O(n）
+ *  空间复杂度：O(k)
+ *
+ *  1. 计算桶？ x为负数时，求哪个桶（由于 Java 的除法，向下取整）
+ *    (x+1)/(w-1)
+ *
+ *     例如 w = 10，非负数桶为 0~9、10~19、20~29
+ *                      -1~-10、-11~-20
+ *     如果 -1~-10 直接除以 10，会被分到两组中
+ *     所以先 +1 变为 0~-9，再除以 10，最后除以 10，都被分到 -1~-10 这一组中
+ *
+ *   2. 滑动窗口
+ *
+ * Created by liujie on 2021/4/25
+ */
+
+public class Solution2 {
+
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        // <桶，元素>
+        Map<Long, Long> map = new HashMap<>();
+        // 桶的大小
+        long w = (long) t + 1;
+        for (int i = 0; i < nums.length; i++) {
+            long x = getId(nums[i], w);
+            // 有两个元素同属一个组，这两个元素必然符合条件
+            if (map.containsKey(x)) {
+                return true;
+            }
+            // 相邻桶
+            if (map.containsKey(x - 1) && Math.abs(nums[i] - map.get(x - 1)) < w) {
+                return true;
+            }
+            if (map.containsKey(x + 1) && Math.abs(nums[i] - map.get(x + 1)) < w) {
+                return true;
+            }
+            map.put(x, (long) nums[i]);
+            // 滑动窗口，保证 abs(i-j) <= k
+            if (i >= k) {
+                map.remove(getId(nums[i - k], w));
+            }
+        }
+        return false;
+    }
+
+    private static long getId(long x, long w) {
+        if (x >= 0) {
+            return x / w;
+        }
+        return (x + 1) / w - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("----- https://leetcode-cn.com/problems/contains-duplicate-iii/ -----");
+        System.out.println("----- 1 -----");
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{1,2,3,1}, 3, 0));
+
+        System.out.println("----- 2 -----");
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{1,0,1,1}, 1, 2));
+
+        System.out.println("----- 3 -----");
+        System.out.println(containsNearbyAlmostDuplicate(new int[]{1,5,9,1,5,9}, 2, 3));
+
+    }
+}
