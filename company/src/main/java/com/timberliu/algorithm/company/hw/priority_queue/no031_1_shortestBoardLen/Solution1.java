3fca5cd14ad59c7e27caff4f1fdcd28ab4c58aa5
liujie0813 <18220747087@163.com>
2023-04-25 23:53:49 +0800

    hw - 30、31 - 2 - 411

---
+++ company/src/main/java/com/timberliu/algorithm/company/hw/priority_queue/no031_1_shortestBoardLen/Solution1.java
@@ -0,0 +1,47 @@
+package com.timberliu.algorithm.company.hw.priority_queue.no031_1_shortestBoardLen;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * 31. 最短木板长度
+ *
+ * @author Timber
+ * @date 2023/4/25
+ */
+public class Solution1 {
+
+	public static int shortestBoardLen(int[] arr, int m) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int val : arr) {
+			pq.offer(val);
+		}
+		for (int i = 0; i < m; i++) {
+			Integer top = pq.poll();
+			pq.offer(top + 1);
+		}
+		return pq.peek();
+	}
+
+	/*
+	 * 5 3
+	 * 4 5 3 5 5
+	 */
+	/**
+	 * 5 2
+	 * 4 5 3 5 5
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		int res = shortestBoardLen(arr, m);
+		System.out.println(res);
+	}
+
+}
