270db56176a1e539b1f5f4b15bccfd64dad5932a
liujie0813 <18220747087@163.com>
2023-04-27 19:10:28 +0800

    hw - 15、16、17、18、19、20、21、22 - 8 - 426

---
+++ company/src/main/java/com/timberliu/algorithm/company/hw/dp/no016_1_maxScoreReportLog/Solution1.java
@@ -0,0 +1,45 @@
+package com.timberliu.algorithm.company.hw.dp.no016_1_maxScoreReportLog;
+
+import java.util.Scanner;
+
+/**
+ * 16. 日志上报首次最多积分
+ *
+ * @author liujie
+ * @date 2023/4/27
+ */
+
+public class Solution1 {
+
+	public static int maxScore(int[] arr) {
+		int max = arr[0];
+		int total = arr[0], delay = 0;
+		for (int i = 1; i < arr.length; i++) {
+			delay += total;
+			total = Math.min(100, total + arr[i]);
+			max = Math.max(max, total - delay);
+			if (total == 100) {
+				break;
+			}
+		}
+		return max;
+	}
+
+	/*
+	 * 1 98 1
+	 */
+	/**
+	 * 3 7 40 10 60
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] strs = scanner.nextLine().split(" ");
+		int[] arr = new int[strs.length];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(strs[i]);
+		}
+		int res = maxScore(arr);
+		System.out.println(res);
+	}
+
+}
